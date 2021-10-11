5eaee924940819213d852d9f2935346af596ae1b
abhishek <anshu98@outlook.com>
2021-10-11 18:09:02 +0530

    Microservice Completed

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/abhishekratnam/go-auth
 go 1.16
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/gofiber/fiber/v2 v2.20.1
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
 	gorm.io/driver/mysql v1.1.2
--- main.go
+++ main.go
@@ -4,11 +4,15 @@ import (
 	"github.com/abhishekratnam/go-auth/database"
 	"github.com/abhishekratnam/go-auth/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	database.Connect()
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+	}))
 	routes.Setup(app)
 
 	app.Listen(":8000")
